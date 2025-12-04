2a2e7d11c94eabb613e1985dacfca6987848eca7
Logan McNatt <logan.m.mcnatt@gmail.com>
2025-12-04 11:58:03 -0700

    .

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -5,8 +5,6 @@
 // You can get this from Terraform output: terraform output api_gateway_url
 
 const API_CONFIG = {
-  // TODO: Update this after running `terraform apply`
-  // Example: 'https://abc123xyz.execute-api.us-east-1.amazonaws.com/dev'
   baseUrl: 'https://a03xn7cqoh.execute-api.us-west-2.amazonaws.com/prod',
   
   endpoints: {
